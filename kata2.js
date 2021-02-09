c55b779cad5fab851ac99fb235c5c3de45549d45
Luke Gartland <lukegartland@gmail.com>
2021-02-09 08:44:33 -1000

    maxtix code added

--- kata2.js
+++ kata2.js
@@ -639,14 +639,43 @@ function vowels(str) {
 // vowels('hello there are some')
 
 function matrix(n) {
-  const result = [];
-    console.log(n);
-    const numOfArrays = n * n
-    console.log(numOfArrays);
-    for (col of n) {
-      result.push(n)
+  const results = [];
+   
+    for (let i = 0; i < n; i++){
+      results.push([])
     }
-    return result
+    let counter =  1;
+    let startColumn = 0
+    let endColumn = n -1
+    let startRow = 0
+    let endRow = n - 1
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+      // top row
+      for (let i = startColumn; i <= endColumn; i++){
+        results[startRow][i]  = counter;
+        counter++;
+      }
+      startRow++
+      // right columns
+      for (let i = startRow; i <= endRow; i++ ){
+        results[i][endColumn] = counter;
+        counter++
+      }
+      endColumn--
+      for (let  i =  endColumn; i >= startColumn; i--){
+        results[endRow][i] = counter;
+        counter++
+      }
+      endRow--
+      //  start columns
+      for  (let i = endRow; i >=  startRow; i--){
+        results[i][startColumn] = counter;
+        counter++
+      }
+      startColumn++
+    }
+  return results
 }
 
 console.log(matrix(2));
