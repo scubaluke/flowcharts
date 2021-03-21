da949109cfff8a4e3264442d8415e04fd50a7218
Luke Gartland <lukegartland@gmail.com>
2021-03-21 08:51:36 -1000

    formating

--- kata2.js
+++ kata2.js
@@ -778,6 +778,5 @@ function hero(bullets, dragons){
   // console.log(moveZeros([ 9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9 ]));
 
   const lovefunc = (flower1, flower2) => flower1 % 2 != flower2 % 2 
-  
-  console.log(lovefunc(1,4));
-  console.log(lovefunc(2,2));
+  // console.log(lovefunc(1,4));
+  // console.log(lovefunc(2,2));
